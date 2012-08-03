dd4cb39bfc81c9918f352541ca4e9f06b27def38
Mike Zauner <mikezauner@gmail.com>
2012-08-03 00:09:37 -0600

    Created 'about' Activity.  Added to AndroidManifest

---
+++ src/com/mikezauner/trailheads/About.java
@@ -0,0 +1,12 @@
+package com.mikezauner.trailheads;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+}
