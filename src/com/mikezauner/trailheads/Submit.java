614ef63181b4cb886fefb4ef3fe8dab91e98c2ee
Mike Zauner <mzauner@ocd.(none)>
2012-08-04 18:13:03 -0600

    Fleshed out submit dialog.

--- src/com/mikezauner/trailheads/Submit.java
+++ src/com/mikezauner/trailheads/Submit.java
@@ -1,15 +1,49 @@
 package com.mikezauner.trailheads;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class Submit extends Activity {
+
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		sendEmail();
+	    LayoutInflater factory = LayoutInflater.from(this);            
+		final View editTextView = factory.inflate(R.layout.layout_submit, null);
+		Context context = this;
+		Button button;
+		EditText name = (EditText) editTextView.findViewById(R.id.editText1);
+		EditText description = (EditText) editTextView.findViewById(R.id.editText3);
+		EditText facilities = (EditText) editTextView.findViewById(R.id.editText4);
+		EditText length = (EditText) editTextView.findViewById(R.id.editText2);
+		
+		AlertDialog.Builder alert= new AlertDialog.Builder(context);
+		alert.create();
+		alert.setTitle("Submit A New Map");
+
+		alert.setView(editTextView);
+        alert.setPositiveButton("Submit",
+                new DialogInterface.OnClickListener() {
+                                 public void onClick(DialogInterface dialog, int whichButton) {
+                                 sendEmail();
+                         }
+                         });
+        alert.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                                 public void onClick(DialogInterface dialog, int whichButton) {
+                                 dialog.cancel();
+                         }
+                         });
+		alert.show();
 	}
 	
 	private void sendEmail() {
